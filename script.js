d4cca34d3ac58e954a719b0cc2dd8c9383bf2f7a
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-11 22:43:41 -0500

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+//react render. Has two pram, 1: what do you want to render
+//2: where do you want to render it. In this case, In a div with id 'root'
+
+ReactDOM.render(<h1>Hello React!</h1>, document.getElementById("root"));
+
+
+//let's render something else
