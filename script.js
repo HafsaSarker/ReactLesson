170499386370aec200ad098d629919b7c4b6da77
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-11 23:41:10 -0500

    update files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//render: to place something on the screen
 //react render. Has two pram, 1: what do you want to render
 //2: where do you want to render it. In this case, In a div with id 'root'
 
@@ -5,3 +6,17 @@ ReactDOM.render(<h1>Hello React!</h1>, document.getElementById("root"));
 
 
 //let's render something else
+ReactDOM.render(<h2>The nigiri</h2>, document.getElementById("root"));
+
+ReactDOM.render(
+    <ul>List<li>Get food</li><li>Finish workout</li></ul>, 
+    document.getElementById("listEx")
+);
+
+
+//creating custom HTML elementrtags
+function bio(){
+    return {
+        
+    }
+}
