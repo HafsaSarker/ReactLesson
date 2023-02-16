2c97f285a2e7716b0dfe2008ea7ad5485c8e373f
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-15 23:31:24 -0500

    new proj

---
+++ businessCard/src/App.jsx
@@ -0,0 +1,16 @@
+import Info from './component/Info';
+// import About from './component/About';
+// import Interests from './component/Interests';
+// import Footer from './component/Footer';
+import './App.css'
+
+export default function App () {
+  return (
+    <>
+      <Info />
+      {/* <About />
+      <Interests />
+      <Footer /> */}
+    </>
+  )
+}
---
+++ businessCard/src/component/Info.jsx
@@ -0,0 +1,18 @@
+import './Info.css';
+import pfp from './person.jpg'
+import { MdEmail } from 'react-icons/md';
+import { BsLinkedin } from 'react-icons/bs';
+export default function Info() {
+    return (
+        <div className='body'>
+            <img src={pfp} className='pfp'/>
+            <h1>Laura Smith</h1>
+            <p className='job'>Frontend Developer</p>
+            <p className='web'>laurasmith.website</p>
+            <div className="buttonContainer">
+                <button><MdEmail /> Email</button>
+                <button><BsLinkedin /> LinkedIn</button>
+            </div>
+        </div>
+    )
+}
