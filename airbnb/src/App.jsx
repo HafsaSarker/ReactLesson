283b08e236efec1bdde14991406b76e7f9e4152f
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-18 22:27:16 -0500

    finish airbnb proj using map method on data

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -2,23 +2,26 @@ import './App.css'
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Card from './components/card/Card';
-import KatieImg from './assets/Katie.png';
+import data from './data';
 
 function App() {
+  const CardMap = data.map((card) => {
+    return <Card 
+      img = {card.coverImg}
+      rating = {card.stats.rating}
+      reviewCount = {card.stats.reviewCount}
+      country = {card.location}
+      title = {card.title}
+      price = {card.price}
+    />
+  });
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-        obj = {{
-          img: KatieImg,
-          rating: 5.0,
-          reviewCount: 6,
-          country: "UK",
-          title: "Life Lessons with Katie Zaferes",
-          price: 136
-        }}
-      />
+      <section className='cards-list'>
+        {CardMap}
+      </section>
     </div>
   )
 }
