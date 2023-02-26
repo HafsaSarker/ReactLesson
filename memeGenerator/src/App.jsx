86bb4159bee4899a101f9858ca43f34980928ad2
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-26 00:31:11 -0500

    add meme component and inputs

--- memeGenerator/src/App.jsx
+++ memeGenerator/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Header from './components/Header'
+import Meme from './components/Meme'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   )
 }
