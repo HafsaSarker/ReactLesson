640fb5718f5d3fb58b33dad6b95e2207f45b5a53
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-02 20:00:32 -0500

    update meme state

--- memeGenerator/src/components/Meme.jsx
+++ memeGenerator/src/components/Meme.jsx
@@ -1,18 +1,35 @@
 import memesData from "../memesData";
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function Meme() {
     const dummyImgUrl = "https://crhscountyline.com/wp-content/uploads/2019/12/Blank-Nut-Button.jpg";
-    const [imgURL, setUrl] = useState(dummyImgUrl);
 
+    const [meme, setMeme] = React.useState({
+        topText : "",
+        bottomText: "",
+        randomImg : dummyImgUrl
+    });
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+
+    
+    
+    // const [imgURL, setUrl] = useState(dummyImgUrl);
+ 
     function getRandomImg(){
 
         //get random img url
         const arr = memesData.data.memes;
-        let rand = Math.floor(Math.random() * (arr.length - 1));
+        let rand = Math.floor(Math.random() * (arr.length));
         const url = arr[rand].url;
         
-        setUrl(url);
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                randomImg: url
+            }
+        });
         
     }
 
@@ -23,7 +40,7 @@ export default function Meme() {
                 <input placeholder="Bottom Text" type="text"></input>
             </form>
             <button onClick={getRandomImg}>New Meme Image</button>
-            <img src={imgURL} />
+            <img src={meme.randomImg} />
         </main>
     )
 }
