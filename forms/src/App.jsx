6078b9f22c7c5a24db808233960d08f3fd7fec59
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-03-08 01:11:52 -0500

    add multi inputs

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [inputs, setInputs] = useState({
+    first: '',
+    last: ''
+  });
+  const handleChange = (event) => {
+    //console.log(event.target.name);
+    setInputs(prevState => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
+    })
+    console.log(inputs);
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <input 
+          type="text" 
+          value={inputs.first} 
+          placeholder='First Name' 
+          name='first'
+          onChange={handleChange}
+        />
+
+        <input 
+          type="text" 
+          value={inputs.last} 
+          placeholder='Last Name' 
+          name='last'  
+          onChange={handleChange}
+        />
+      </form>
     </div>
   )
 }
